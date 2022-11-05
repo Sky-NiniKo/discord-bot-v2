17c99f62576415c9e0d7fff8bc786e8ee19011ba
Sky NiniKo <pcninik@gmail.com>
2022-11-05 20:38:57 +1100

    v2 Beta 5.6.10.9

--- discord_bot_v2/python_script/calculator.py
+++ discord_bot_v2/python_script/calculator.py
@@ -7,15 +7,18 @@ else:
     from .utils import exit_after
 
 
-QALC_PATH = os.environ.get('QALC_PATH', "/usr/bin/qalc")
+if os.environ.get("nix"):
+    QALC_PATH = ["nix-shell", "-p", "qalc"]
+else:
+    QALC_PATH = [os.environ.get('QALC_PATH', "/usr/bin/qalc")]
 
 
 @exit_after(10)
 def calculate(*args, calculation: str) -> str:
     if calculation:
-        p = subprocess.Popen([QALC_PATH, calculation], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen([*QALC_PATH, calculation], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     else:
-        p = subprocess.Popen([QALC_PATH, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen([*QALC_PATH, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return p.stdout.read().decode().strip()
 
 
